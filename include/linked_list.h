cdec68b5c84b187830eeba199a37d09074de322b
Eugene <yuchunlin0075@gmail.com>
2021-03-10 21:08:49 +0800

    Add comment on LinkedList class

--- include/linked_list.h
+++ include/linked_list.h
@@ -1,7 +1,7 @@
 /** 
  * @file linked_list.h
  * @brief linked list definition
- * @author Eugene Lin
+ * @author Eugene Lin <lin.eugene.l.e@gmail.com>
  * @date 2021.2.23
  */
 #ifndef __LINKED_LIST_H__
@@ -10,23 +10,74 @@
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
 
+
+/** @class LinkedList
+ *	@brief Class for linked list data structure.
+ */
 class LinkedList{
 friend class TestLinkedList;
 private:
 	LinkedList * next;
 	LinkedList * prev;
 public:
+	/** @brief Construct LinkedList object 
+	 */
 	__host__ LinkedList();
+
+	
+	/** @brief Get the next LinkedList element
+	 *
+	 * 	@return LinkedList * or NULL
+	 *	
+	 *	@details
+	 *	Get the next LinkedList element. If LinkedList element is not connected
+	 *	to next element the function will return NULL.
+	 */
 	__device__ __host__ LinkedList * getNext();
+
+	/**	@brief Get the previous LinkedList element
+	 *	@return LinkedList * or NULL
+	 *	@details
+	 *	Get the previous LinkedList element. If LinkedList element is not connected
+	 *	to previous element the function will return NULL.
+	 */
 	__device__ __host__ LinkedList * getPrev();
-	__device__ __host__ void setNext(LinkedList *);
+
+	/**	@brief Set the next element of the LinkedList
+	 *	@return Void
+	 *	@param next next element
+	 *	@details
+	 *	Connect to next LinkedList element.
+	 */
+	__device__ __host__ void setNext(LinkedList *next);
+
+	/**	@brief Set the previous element of the LinkedList
+	 * 	@return Void
+	 * 	@param prev previous element
+	 *
+	 * 	@details
+	 * 	Connect to previous LinkedList element
+	 */
 	__device__ __host__ void setPrev(LinkedList *);
+	
+	/**	@brief Get the value
+	 * 	@return value the value of LinkedList
+	 */
 	virtual __device__ __host__ double getValue()=0;
+
+	
+	/**	@brief	Destruct the object
+	 */
 	virtual ~LinkedList();
 };
 
-__device__ __host__ LinkedList * linkedListMergeSort(LinkedList *);
+/**	@brief merge sort of LinkedList 
+ * 	@return LinkedList* return the head of sorted list
+ *
+ * 	@param head head of a linked list
+ */
+__device__ __host__ LinkedList * linkedListMergeSort(LinkedList *head);
 
-__global__ void vectorAddInt(int * a, int *b, int *c, unsigned int num_elements);
+// __global__ void vectorAddInt(int * a, int *b, int *c, unsigned int num_elements);
 
 #endif
--- lib/linked_list.cu
+++ lib/linked_list.cu
@@ -25,7 +25,8 @@ __device__ __host__ LinkedList * mergeLinkedList(LinkedList * l1, LinkedList * l
 	if(!l1) return l2;
 	
 	LinkedList * result, *result_iter;
-
+	
+	// set the first element of result
 	if(l1->getValue() < l2->getValue()) {
 		result = l1;
 		l1 = l1->getNext();	
@@ -35,18 +36,20 @@ __device__ __host__ LinkedList * mergeLinkedList(LinkedList * l1, LinkedList * l
 	}
 	
 	result_iter = result;
-
+	
+	// merge the linked list
 	while(l1 && l2) {
 		if(l1->getValue() < l2->getValue()){
-			result_iter->setNext(l1);
-			l1 = l1->getNext();
+			result_iter->setNext(l1); // connect to next element
+			l1 = l1->getNext(); // l1 move to next element
 		} else{
 			result_iter->setNext(l2);
-			l2 = l2->getNext();
+			l2 = l2->getNext(); // l2 move to next element
 		}
-		result_iter = result_iter->getNext();
+		result_iter = result_iter->getNext(); // point to next element
 	}
-
+	
+	// if l1 is not empty, connect to result
 	if(l1) result_iter->setNext(l1);
 	else if(l2) result_iter->setNext(l2);
 
@@ -60,27 +63,33 @@ __device__ __host__ LinkedList * linkedListMergeSort(LinkedList * head){
 
 	LinkedList *fast = head->getNext();
 	LinkedList *slow = head;
+	
+	// get the middle of linked list
+	// divide the linked list
 	while(fast && fast->getNext()){
 		slow = slow->getNext();
 		fast = fast->getNext()->getNext();
 	}
+	// now, get two lists.
 	fast = slow->getNext();
 	slow->setNext(NULL);
+	
 
 	LinkedList *lhs = linkedListMergeSort(head);
 	LinkedList *rhs = linkedListMergeSort(slow);
-
+	
+	// merge the linked list
 	return mergeLinkedList(lhs, rhs);	
 
 }
 
 
-__global__ void vectorAddInt(int * a, int *b, int *c, unsigned int num_elements){
-	unsigned int idx = blockDim.x * blockIdx.x + threadIdx.x;
-	if(idx < num_elements){
-		c[idx] = a[idx] + b[idx];
-	}
-}
+// __global__ void vectorAddInt(int * a, int *b, int *c, unsigned int num_elements){
+// 	unsigned int idx = blockDim.x * blockIdx.x + threadIdx.x;
+// 	if(idx < num_elements){
+// 		c[idx] = a[idx] + b[idx];
+// 	}
+// }
 
 LinkedList::~LinkedList(){
 
