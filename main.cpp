6882d4cb0d99d14b5aa88678177a5eee8cf34012
Eugene <yuchunlin0075@gmail.com>
2021-08-16 17:18:37 +0800

    Create lots by using lots.csv

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <ctime>
 #include <map>
 #include <string>
@@ -17,42 +18,27 @@ void output(population_t *pop, csv_t *csv);
 
 void outputJobInMachine(map<string, machine_t *>, csv_t *csv);
 
+lots_t createLots(int argc, const char *argv[]);
+
+entities_t createEntities(int argc, const char *argv[]);
+
 int main(int argc, const char *argv[])
 {
     if (argc < 2) {
         printf("Please specify the path of configuration file\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     csv_t cfg(argv[1], "r", true, true);
     map<string, string> arguments = cfg.getElements(0);
 
-    lots_t lots;
-    lots.createLots(arguments);
+    lots_t lots = createLots(argc, argv);
+
 
     ancillary_resources_t tools(lots.amountOfTools());
     ancillary_resources_t wires(lots.amountOfWires());
 
-    csv_t machine_csv(arguments["machines"], "r", true, true);
-    machine_csv.trim(" ");
-    machine_csv.setHeaders(map<string, string>({{"entity", "ENTITY"},
-                                                {"model", "MODEL"},
-                                                {"recover_time", "OUTPLAN"},
-                                                {"prod_id", "PRODUCT"},
-                                                {"pin_pkg", "PIN_PKG"},
-                                                {"lot_number", "LOT#"},
-                                                {"customer", "CUST"},
-                                                {"bd_id", "BOND ID"},
-                                                {"oper", "OPER"}}));
-
-    csv_t location_csv(arguments["locations"], "r", true, true);
-    location_csv.trim(" ");
-    location_csv.setHeaders(
-        map<string, string>({{"entity", "Entity"}, {"location", "Location"}}));
-
-
-    entities_t entities(arguments);
-    entities.addMachines(machine_csv, location_csv);
+    entities_t entities = createEntities(argc, argv);
     machines_t machines;
     machines.addMachines(entities.getAllEntity());
 
@@ -85,29 +71,82 @@ int main(int argc, const char *argv[])
                  }},
     };
 
-    csv_t result("output/result.csv", "w");
-    outputJobInMachine(machines.getMachines(), &result);
-    initializeOperations(&pop);
+    // csv_t result("output/result.csv", "w");
+    // outputJobInMachine(machines.getMachines(), &result);
+    // initializeOperations(&pop);
 
-    int i = 0;
-    while (lots.toolWireLotsHasLots()) {
-        printf("i = %d\n", i++);
-        pop.groups = lots.round(entities);
-        // if(pop.groups.size() == 0){
-        //     continue;
-        // }
-        initializePopulation(&pop, machines, tools, wires);
-        geneticAlgorithm(&pop);
-        // optimization(&pop);
-        output(&pop, &result);
-        machineWriteBackToEntity(&pop);
-        freeJobs(&pop.round);
-        freeResources(&pop.round);
-        freeChromosomes(&pop);
-    }
-    result.write();
+    // int i = 0;
+    // while (lots.toolWireLotsHasLots()) {
+    //     printf("i = %d\n", i++);
+    //     pop.groups = lots.round(entities);
+    //     // if(pop.groups.size() == 0){
+    //     //     continue;
+    //     // }
+    //     initializePopulation(&pop, machines, tools, wires);
+    //     geneticAlgorithm(&pop);
+    //     // optimization(&pop);
+    //     output(&pop, &result);
+    //     machineWriteBackToEntity(&pop);
+    //     freeJobs(&pop.round);
+    //     freeResources(&pop.round);
+    //     freeChromosomes(&pop);
+    // }
+    // result.write();
 
     return 0;
+}
+
+lots_t createLots(int argc, const char *argv[])
+{
+    csv_t cfg(argv[1], "r", true, true);
+    map<string, string> arguments = cfg.getElements(0);
+
+    lots_t lots;
+    if (argc >= 3) {
+        printf("Create lots by using pre-created lots.csv file : %s\n",
+               argv[2]);
+        csv_t lots_csv(argv[2], "r", true, true);
+        vector<lot_t> all_lots;
+        for (int i = 0, nrows = lots_csv.nrows(); i < nrows; ++i) {
+            printf("Entry[%d]\n", i);
+            lot_t lot(lots_csv.getElements(i));
+            all_lots.push_back(lot);
+        }
+        lots.addLots(all_lots);
+    } else {
+        printf("Create lots by using configure file : %s\n", argv[1]);
+        lots.createLots(arguments);
+    }
+
+    return lots;
+}
+
+entities_t createEntities(int argc, const char *argv[])
+{
+    csv_t cfg(argv[1], "r", true, true);
+    map<string, string> arguments = cfg.getElements(0);
+
+    csv_t machine_csv(arguments["machines"], "r", true, true);
+    machine_csv.trim(" ");
+    machine_csv.setHeaders(map<string, string>({{"entity", "ENTITY"},
+                                                {"model", "MODEL"},
+                                                {"recover_time", "OUTPLAN"},
+                                                {"prod_id", "PRODUCT"},
+                                                {"pin_pkg", "PIN_PKG"},
+                                                {"lot_number", "LOT#"},
+                                                {"customer", "CUST"},
+                                                {"bd_id", "BOND ID"},
+                                                {"oper", "OPER"}}));
+
+    csv_t location_csv(arguments["locations"], "r", true, true);
+    location_csv.trim(" ");
+    location_csv.setHeaders(
+        map<string, string>({{"entity", "Entity"}, {"location", "Location"}}));
+
+
+    entities_t entities(arguments);
+    entities.addMachines(machine_csv, location_csv);
+    return entities;
 }
 
 map<string, string> outputJob(job_t job)
