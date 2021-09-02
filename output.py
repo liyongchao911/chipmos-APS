c3f0bd1cf44f1caeff2d3ac4fe9b376d61d651a6
梁哲榕 <zrliang1205@gmail.com>
2021-09-02 18:13:25 +0800

    Update simulation & setup

--- output.py
+++ output.py
@@ -72,87 +72,7 @@ def plot_gantt_by_order(result_df):
 
     pass
 
-def plot_gantt_by_sets(result_df):
-    #1. Group by sets to find the entity order to plot
-    def createFolder(directory):
-        try:
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-        except OSError:
-            print ('Error: Creating directory. ' +  directory) 
-
-    plot_num =50 #each time plot 50 entities
-    sets= result_df.groupby(['part_no','part_id','entity']) # group by sets
-    entities= result_df.groupby('entity') # group by entity
-    sets_key_list= list(sets.groups.keys()) 
-    entity_key_list= list(entities.groups.keys())
-    color_discrete_sequence_list=['#F0F8FF','#7FFFD4','#F0FFFF','#F5F5DC','#FFE4C4',
-                '#000000','#0000FF','#8A2BE2','#A52A2A','#DEB887','#5F9EA0',
-                '#7FFF00','#D2691E','#FF7F50','#6495ED','#FFF8DC','#DC143C','#00FFFF',
-                '#00008B','#008B8B','#B8860B','#A9A9A9','#006400','#BDB76B','#8B008B',
-                '#556B2F','#FF8C00','#9932CC','#8B0000','#E9967A','#8FBC8F','#483D8B',
-                '#2F4F4F','#00CED1','#9400D3','#FF1493','#00BFFF','#696969','#1E90FF',
-                '#B22222','#FFFAF0','#228B22','#FF00FF','#DCDCDC','#F8F8FF','#FFD700',
-                '#DAA520','#808080','#008000','#ADFF2F','#F0FFF0','#FF69B4','#CD5C5C',
-                '#4B0082','#FFFFF0','#F0E68C','#E6E6FA','#FFF0F5','#7CFC00','#FFFACD',
-                '#ADD8E6','#F08080','#E0FFFF','#FAFAD2','#90EE90','#D3D3D3','#FFB6C1',
-                '#FFA07A','#20B2AA','#87CEFA','#778899','#B0C4DE','#FFFFE0','#00FF00',
-                '#32CD32','#FAF0E6','#FF00FF','#800000','#66CDAA','#0000CD','#BA55D3',
-                '#9370DB','#3CB371','#7B68EE','#00FA9A','#48D1CC','#C71585','#191970',
-                '#F5FFFA','#FFE4E1','#FFE4B5','#FFDEAD','#000080','#FDF5E6','#808000',
-                '#6B8E23','#FFA500','#FF4500','#DA70D6','#EEE8AA','#98FB98','#AFEEEE',
-                '#DB7093','#FFEFD5','#FFDAB9','#CD853F','#FFC0CB','#DDA0DD','#B0E0E6',
-                '#800080','#FF0000','#BC8F8F','#4169E1','#8B4513','#FA8072','#FAA460',
-                '#2E8B57','#FFF5EE','#A0522D','#C0C0C0','#87CEEB','#6A5ACD','#708090',
-                '#FFFAFA','#00FF7F','#4682B4','#D2B48C','#008080','#D8BFD8','#FF6347',
-                '#40E0D0','#EE82EE','#F5DEB3','#FFFFFF','#F5F5F5','#FFFF00','#9ACD32']
-    df=[] #plot need dataframe
-    onetime_plot_entity_order=[] # Record each time entity order to plot to update yaxes
-    plt_count=0 # record the times current plot
-    plt_name_count=0 #output file name need
-    plot_entity_order = []   #record entity from first set to the last (not repeating).
-    for i in range(len(sets_key_list)):
-        if sets_key_list[i][2] not in plot_entity_order:
-            plot_entity_order.append(sets_key_list[i][2])
-
-    foldername = './output/plot_by_sets' + '/' # folder which store result picture
-    createFolder(foldername)
-
-    # plot
-    for i in range(len(plot_entity_order)): 
-        key= entity_key_list[entity_key_list.index(plot_entity_order[i])]  # find index of machine_order on entity_key_list (ex:BB568 >137)
-    #   key= machine_order[i]  #test
-        onetime_plot_entity_order.append(key)
-        plt_count+=1
-    
-        for j in range(len(entities.groups[key])):
-            row_index=entities.groups[key][j]
-
-            Lot_id =result_df.iloc[row_index].at['lot_number'].strip() # remove space
-            Entity =result_df.iloc[row_index].at['entity'].strip()
-            start_ts =  time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ini_time_stamp+result_df.iloc[row_index].at['start_time']*60)) 
-            end_ts =  time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ini_time_stamp+result_df.iloc[row_index].at['end_time']*60)) 
-            Sets = result_df.iloc[row_index].at['part_no'].strip()+ "/"+ result_df.iloc[row_index].at['part_id'].strip()
-            Bd_id= result_df.iloc[row_index].at['bd_id'].strip()
-            df.append(dict(Lot_ID= Lot_id, Start=start_ts,Finish=end_ts,Entity=Entity,Sets=Sets,Bd_Id=Bd_id))
-
-        if(plt_count==plot_num) or i==len(plot_entity_order)-1: # consider final part
-        #show figure
-            plt_name_count+=1
-            fig = px.timeline(df, x_start="Start", x_end="Finish", y="Entity", color="Sets",color_discrete_sequence=color_discrete_sequence_list,text="Lot_ID",hover_name="Bd_Id")   # hover_name
-            fig.update_yaxes(categoryorder="array", categoryarray= onetime_plot_entity_order) # plot by onetime_plot_entity_order
-            fig.update_traces(textposition='inside',marker_line_color='rgb(8,48,107)')
-            fig.write_html(foldername + str(plt_name_count) +".html")
-            # reset
-            df=[]
-            plt_count=0
-            onetime_plot_entity_order=[]
-
-    pass
-
 def output_simulation(result_df,machine_area_df): 
-    #1. Need to find the amount of entity(ENT) which use same sets('cust','pin_pkg','prod_id','bd_id','Location') on different moment and total die quantity(outplan).
-    #2. Detail can be found on each function
 
     #  Get the filetered dataframe by judging whether proceesing through specific time
     def get_result_byFilter(merge_result,relative_time):
@@ -175,13 +95,15 @@ def output_simulation(result_df,machine_area_df):
         ENTdf= pd.concat([df_keys, df_Ent_amount], axis=1)
         ENTdf.columns=['cust','pin_pkg','prod_id','bd_id','oper','Location',ent_colname]  
 
+
         return ENTdf
     
     #Use group by to get the amount of quantity of each sets and make it to dataframe(list>df)
     #Find the lots using same group by set and accumulate their die quantity
-    def get_qty_df(original_group,merge_result):
+    def get_qty_df(merge_result):
 
         qty_list=[]
+        original_group=merge_result.groupby(['cust','pin_pkg','prod_id','bd_id','oper','Location']) 
         group_key_list= list(original_group.groups.keys())
         
         for i in range(len(group_key_list)): 
@@ -192,44 +114,44 @@ def output_simulation(result_df,machine_area_df):
                 qty_sum+=merge_result.iloc[row_index].at['qty']
             qty_list.append(qty_sum)
 
-        qty_df = pd.DataFrame(qty_list,columns=['Output plan'])
+        df_keys= pd.DataFrame(original_group.groups.keys())
+        qty_df = pd.DataFrame(qty_list)
+        outplan_df= pd.concat([df_keys, qty_df], axis=1)
+        outplan_df.columns=['cust','pin_pkg','prod_id','bd_id','oper','Location','Output plan'] 
 
-        return qty_df
+        return outplan_df
 
     # initialize
-    merge_result = pd.merge(result_df, machine_area_df, left_on ='entity', right_on ='Entity', how='left')
-    original_group=merge_result.groupby(['cust','pin_pkg','prod_id','bd_id','oper','Location']) 
+    merge_result = pd.merge(result_df, machine_area_df, left_on ='entity', right_on ='Entity', how='left') 
+    oneday_result = pd.merge(result_df[result_df["end_time"]<= 1440], machine_area_df, left_on ='entity', right_on ='Entity', how='left') #(total 27.5hrs)
 
-    ENT_all = get_ENT_df(merge_result[merge_result["end_time"] <= 1650],'Allocate ENT')  # the lots from 07:30 today to 11:00 tomorrow (total 27.5hrs)
-    ENT_1 = get_ENT_df(get_result_byFilter(merge_result,0),'Original ENT(07:30)')      #relative to 07:30(standard time) is 0 minute
-    ENT_2 = get_ENT_df(get_result_byFilter(merge_result,210),'Original ENT(11:00)')    #relative to 11:00 is need to plus 210 minute
-    qty_df = get_qty_df(original_group,merge_result)
+    grp_basis = merge_result.groupby(['cust','pin_pkg','prod_id','bd_id','oper','Location']) 
+    df_frame= pd.DataFrame(grp_basis.groups.keys())
+    df_frame.columns=['cust','pin_pkg','prod_id','bd_id','oper','Location']  
 
+    ENT_1 = get_ENT_df(get_result_byFilter(merge_result,0),'Original ENT(+0)')      #relative to 07:30(standard time) is 0 minute
+    ENT_2 = get_ENT_df(get_result_byFilter(merge_result,210),'Original ENT(+210)')    #relative to 11:00 is need to plus 210 minute
+    ENT_3 = get_ENT_df(get_result_byFilter(merge_result,1650),'Allocate ENT')
+    outplan_df = get_qty_df(oneday_result)                                          # one day
 
     # merge and concat all data to one dataframe
-    merge_df1 = pd.merge(ENT_all, ENT_1, on =(['cust','pin_pkg','prod_id','bd_id','oper','Location']), how ='left')
+    merge_df1 = pd.merge(df_frame, ENT_1, on =(['cust','pin_pkg','prod_id','bd_id','oper','Location']), how ='left')
     merge_df2 = pd.merge(merge_df1, ENT_2, on =(['cust','pin_pkg','prod_id','bd_id','oper','Location']), how ='left')
-    temp_df= pd.concat([merge_df2, qty_df], axis=1)
-    ## change column sequence to create final version
-    simulation_output_df = temp_df[['cust','pin_pkg','prod_id','bd_id','oper','Location','Original ENT(07:30)','Original ENT(11:00)','Allocate ENT','Output plan']]
-
+    merge_df3 = pd.merge(merge_df2, ENT_3, on =(['cust','pin_pkg','prod_id','bd_id','oper','Location']), how ='left')
+    simulation_output_df = pd.merge(merge_df3, outplan_df, on =(['cust','pin_pkg','prod_id','bd_id','oper','Location']), how ='left')
+    
     # write to csv
     simulation_output_df.to_csv("output/simulation_output.csv", index=False ,na_rep=0) 
-
+    
     pass
 
 def output_setup_record(result_df):
-    # 1. Sort the result.csv by entity & start_time
-    # 2. Record the setup of each entity by judging whether the bd_id is same or not (for i in range..)
-    # 3. The method of geting setuptime is recordimg previous endT and current startT(need to change to date)
-    # 4. The format of ouyput is using dictionary to record each entity(key) and its setup information(value) p.s. maybe 0 or more values of each entity record.
-    # 5. Finally, convert dict to csv. 
-
-    sorted_result_df=result_df.sort_values(by=['entity', 'start_time'])
+    sorted_result_df=result_df[result_df["start_time"]<= 1440].sort_values(by=['entity', 'start_time']) #within one day
     setup_dict ={}
     times_info_list=[]
-    entity_name= sorted_result_df['entity'].iloc[0]
-    bd_id = sorted_result_df['bd_id'].iloc[0]
+    entity_name= sorted_result_df['entity'].iloc[0] # default
+    bd_id = sorted_result_df['bd_id'].iloc[0]       # default
+    total_setup_times=0
 
     for i in range(len(sorted_result_df.index)): # serach for all data
         if entity_name == sorted_result_df['entity'].iloc[i]:  # If entity is same, need to judge whether bd_id is same. Else, record new entity(key)
@@ -239,11 +161,13 @@ def output_setup_record(result_df):
                 setup_end=time.strftime("%m-%d %H:%M:%S", time.localtime(ini_time_stamp+sorted_result_df['start_time'].iloc[i]*60))   #the startT of current and convert to date
                 # change to date
                 times_info_list.append(setup_start+' > '+setup_end) 
+                total_setup_times +=1
                 bd_id = sorted_result_df['bd_id'].iloc[i]
 
         else: 
             #add to dict
             setup_dict[entity_name]=times_info_list
+
             #next entitys
             entity_name = sorted_result_df['entity'].iloc[i]
             bd_id = sorted_result_df['bd_id'].iloc[i]
@@ -254,14 +178,15 @@ def output_setup_record(result_df):
             setup_dict[entity_name]=times_info_list
 
     # write file
-    with open("output/setup_record.csv", "w", newline="") as f_output:
+    with open("output/setup_record_0901.csv", "w", newline="") as f_output:
         csv_output = csv.writer(f_output)
+        csv_output.writerow(['一日改機次數',total_setup_times])
         for key in sorted(setup_dict.keys()):
             csv_output.writerow([key] + setup_dict[key])
 
     pass
 
-def output_new_result(result_df):
+def output_result_to_date(result_df):
     # 1. Convert relative time to absulute time(date) (start time and end time)
     # 2. Change the column order
     new_result_df = result_df.copy(deep=True)
@@ -283,7 +208,6 @@ ini_time_stamp = int(time.mktime(time.strptime(config_df.at[0,'std_time'], "%Y/%
 
 # execute
 plot_gantt_by_order(result_df)
-plot_gantt_by_sets(result_df)
 output_simulation(result_df,machine_area_df)
 output_setup_record(result_df)
-output_new_result(result_df)
+output_result_to_date(result_df)
