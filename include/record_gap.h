221ea1af3563e4ea8767aa9d5d6734fc52b102c4
cantfindagoodname <f74087060@gs.ncku.edu.tw>
2021-12-19 14:43:49 +0800

    Update output function, removed no setup time case
    Instead of using predefined functions from fstream library,
    the csv output is generated using written csv functions
    that conform to the format of .csv files
    The output also no longer shows jobs which machines do not
    undergo additional setup process

--- include/record_gap.h
+++ include/record_gap.h
@@ -1,12 +1,11 @@
 #ifndef __RECORD_GAP_H__
 #define __RECORD_GAP_H__
 
-#include <fstream>
-#include <iostream>
 #include <map>
 #include <string>
 #include <vector>
 
+#include "include/csv.h"
 #include "include/info.h"
 #include "include/job.h"
 #include "include/machine.h"
@@ -20,8 +19,11 @@ class Record_gap
 {
 private:
     std::map<std::string, std::vector<job_t *>> _jobs;
-    std::ofstream outputFile;
     machine_base_operations_t *ops;
+
+    std::string directory_name;
+    csv_t csv_file;
+
     std::string str;
     typedef struct name_function_pair_t {
         char name[4];
@@ -36,7 +38,9 @@ private:
 
 public:
     double const threshold;
-    Record_gap(machine_base_operations_t *ops, double th = 0.00000001);
+    Record_gap(machine_base_operations_t *ops,
+               double th = 0.00000001,
+               std::string dir_name = /*"output_0-0"*/ "");
     void addJob(job_t *job);
     void record_gap_all_machines();
 };
