a0a2bfcba791a8f49da55aeda92e4fb39e0d7059
cantfindagoodname <f74087060@gs.ncku.edu.tw>
2022-02-25 14:32:19 +0800

    Change some variable names for config
    Previously, lot.h will find if "PXX" is substring of _lot_number,
    after this commit, it now find if "XX" is the second and third
    character of _lot_number.
    As I am unsure if the program wants to match only the second and
    third character, the find substring version of "XX" is left as
    a comment above the current implementation
    
    In main.cpp, the argument "OPER" is changed to "SETTING OPER" as
    requested. Typo "process_tsime" to "process_time" is fixed as well.
    
    In lot.cpp, there is a request to match _last_entity with "DA", and
    if it is "DA" assign WB_move_in_entity to _last_entity. As the
    request is currently not clear enough, the implementation currently
    is wrapped as comment.

--- src/lot.cpp
+++ src/lot.cpp
@@ -159,6 +159,13 @@ lot_t::lot_t(std::map<std::string, std::string> elements) : _last_location("")
     _wb_location = elements["wb_location"];
     _pkg_id = elements["package_id"];
     _last_entity = tmp_oper == 2070 ? "DA" : elements["last_WB_entity"];
+
+    /* FIXME
+    if(_last_entity.compare("DA") == 0 && elements["WB_move_in_entity"].length()
+    != 0){ _last_entity = elements["WB_move_in_entity"];
+    }
+    */
+
     _wlot_last_trans = elements["wlot_last_trans"];
 
     setHold(elements["hold"]);
