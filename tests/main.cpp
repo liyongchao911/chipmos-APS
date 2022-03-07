1e89de3da2d9e4a47d2d773376fc3e46a0eb7276
Eugene Lin <lin.eugene.l.e@gmail.com>
2022-03-07 13:46:52 +0800

    Initialize time zone in unit test
    In previous version, time zone is not set in tests/main.cpp
    so that the unit test failed in the github action because
    the test suit use GMT+8 unix-time stamp for testing the
    object timeConverter. However, the machine of github action
    running the test may not be in the time zone GMT+8 so that
    the test failed very often. In this version, I fix it.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,7 +1,11 @@
 #include <gtest/gtest.h>
+#include <cstdlib>
 
 int main(int argc, char **argv)
 {
+    // initialize the time zone for testing
+    setenv("TZ", "UTC-8", 1);
+    tzset();
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
