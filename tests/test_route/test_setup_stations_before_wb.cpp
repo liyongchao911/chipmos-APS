88a8844cb182416c48bf8b4822df529820f7f8b3
Eugene Lin <lin.eugene.l.e@gmail.com>
2022-03-18 14:50:35 +0800

    Accelerate testing route and rename file
    In previous commit, the test suit open file in each test case
    and it actually takes a lot of time. In this commit, I write
    SetUpTestSuite function and which is declared with static function.
    The function will only run once in the begining for the test suite.

--- tests/test_route/test_setup_stations_before_wb.cpp
+++ tests/test_route/test_setup_stations_before_wb.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <gtest/internal/gtest-internal.h>
 
 #include "include/csv.h"
 
@@ -15,30 +16,56 @@
 
 using namespace std;
 
+namespace route
+{
 struct route_test_case_t {
     string test_route_name;
     vector<int> test_opers;
+    friend ostream &operator<<(ostream &out, struct route_test_case_t cs)
+    {
+        out << "route name :" << cs.test_route_name << "->";
+        out << ::testing::PrintToString(cs.test_opers);
+        return out;
+    }
 };
 
 
-
-class test_route_t : public testing::TestWithParam<route_test_case_t>
+class test_route_t : public testing::WithParamInterface<route_test_case_t>,
+                     public testing::Test
 {
 public:
     route_t *route;
-    csv_t __routelist;
-    test_route_t() : __routelist("test_data/route_list.csv", "r", true, true)
+    static csv_t *__routelist;
+    test_route_t()
     {
-        __routelist.setHeaders({{"route", "wrto_route"},
-                                {"oper", "wrto_oper"},
-                                {"seq", "wrto_seq_num"},
-                                {"desc", "wrto_opr_shrt_desc"}});
         route = new route_t();
-        route->setRoute(__routelist);
+        route->setRoute(*test_route_t::__routelist);
     }
 
     ~test_route_t() { delete route; }
+
+    static void SetUpTestSuite()
+    {
+        if (__routelist == nullptr) {
+            test_route_t::__routelist =
+                new csv_t("test_data/route_list.csv", "r", true, true);
+            test_route_t::__routelist->setHeaders(
+                {{"route", "wrto_route"},
+                 {"oper", "wrto_oper"},
+                 {"seq", "wrto_seq_num"},
+                 {"desc", "wrto_opr_shrt_desc"}});
+        }
+    }
+
+    static void TearDownTestSuite()
+    {
+        if (__routelist != nullptr) {
+            delete test_route_t::__routelist;
+            __routelist = nullptr;
+        }
+    }
 };
+csv_t *test_route_t::__routelist = nullptr;  // initialization __routelist
 
 
 TEST_P(test_route_t, test_if_oper_in_WB_7)
@@ -47,9 +74,13 @@ TEST_P(test_route_t, test_if_oper_in_WB_7)
     set<int> route_list = route->_beforeWB[cs.test_route_name];
     vector<int> &test_oper = cs.test_opers;
 
-    EXPECT_EQ(route_list.size(), test_oper.size());
+    ASSERT_EQ(route_list.size(), test_oper.size())
+        << cs << ", route list : " << ::testing::PrintToString(route_list)
+        << endl;
     for (int i = 0; i < test_oper.size(); ++i) {
-        EXPECT_EQ(route_list.count(test_oper[i]), 1);
+        ASSERT_EQ(route_list.count(test_oper[i]), 1)
+            << cs << ", route list : " << ::testing::PrintToString(route_list)
+            << endl;
     }
 }
 
@@ -76,3 +107,4 @@ INSTANTIATE_TEST_SUITE_P(
                            2140, 3330, 3340, 3150, 3200, 3205, 3210, 4130, 4330,
                            4340, 3350, 3400, 3205, 3210, 4130, 4330, 4340, 3350,
                            3400, 3405, 3410, 4530, 4730, 4740, 3550, 3600}}));
+}  // namespace route
