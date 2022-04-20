c871cf0a89259e29730921395dfbc57227f423d5
Eugene Lin <lin.eugene.l.e@gmail.com>
2022-04-20 13:58:50 +0800

    Finish half of distributing machines algorithm
    I commit it for  a simple experiment to see if the algorithm indeed
    improve the scheduling performance.

--- src/machines.cpp
+++ src/machines.cpp
@@ -11,7 +11,9 @@
 #include <algorithm>
 #include <cstdlib>
 #include <cstring>
+#include <ctime>
 #include <iostream>
+#include <random>
 #include <set>
 #include <stdexcept>
 #include <vector>
@@ -798,6 +800,9 @@ bool machines_t::_canJobRunOnTheMachine(job_t *job,
     string cust(job->customer.data.text);
     string entity_name(machine->base.machine_no.data.text);
 
+    bool ret = true, care = true;
+    ret = _mcs_a->isMachineRestrained(job, machine, &care);
+
     return _isMachineLocationAvailableForJob(lot_number, location) &&
            (!strict_model || _isModelAvailableForJob(lot_number, model)) &&
            !_isMachineDedicatedForJob(lot_number, cust, entity_name) &&
@@ -831,77 +836,154 @@ void machines_t::_chooseMachinesForAGroup(
     bad_jobs = group->orphan_jobs;
     group->orphan_jobs.clear();
 
-    candidate_machines = _sortedMachines(candidate_machines);
+    // candidate_machines = _sortedMachines(candidate_machines);
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(candidate_machines.begin(), candidate_machines.end(), g);
 
-    map<string, vector<string>> suitable_machines;
 
     int number_of_tools = group->number_of_tools;
     int number_of_wires = group->number_of_wires;
     string part_id = group->part_id;
     string part_no = group->part_no;
 
-
     vector<string> can_run_machines;
 
     // go through the machines
     // until running out of tools or wires
     int i = 0;
-    for (i = 0; i < candidate_machines.size() && number_of_tools > 0 &&
-                number_of_wires > 0;
+    // in this for-loop I am gonna determine the minimum set of available
+    // machines
+    set<machine_t *> minimum_available_machine_set;
+    for (i = 0; i < candidate_machines.size() && number_of_wires > 0 &&
+                number_of_tools > 0;
          ++i) {
         vector<job_t *> nbad_jobs;
-        vector<job_t *> ngood_jobs;
-        foreach (bad_jobs, j) {
+        bool selected = false;
+        string machine_no(candidate_machines[i]->base.machine_no.data.text);
+        for (int j = 0; j < bad_jobs.size(); ++j) {
             string lot_number(bad_jobs[j]->base.job_info.data.text);
             if (_canJobRunOnTheMachine(bad_jobs[j], candidate_machines[i])) {
-                _job_can_run_machines[lot_number].push_back(
-                    string(candidate_machines[i]->base.machine_no.data.text));
-
-                suitable_machines[lot_number].push_back(
-                    string(candidate_machines[i]->base.machine_no.data.text));
-
-                ngood_jobs.push_back(bad_jobs[j]);
+                good_jobs.push_back(bad_jobs[j]);
+                _job_can_run_machines[lot_number].push_back(machine_no);
+                selected = true;
             } else {
                 nbad_jobs.push_back(bad_jobs[j]);
             }
         }
 
-        string model_name(candidate_machines[i]->base.machine_no.data.text);
-        bool used = false;  // a flag to describe if the machine is choose below
-        // if ngood_jobs has jobs means that the machine is a good machine
-        if (ngood_jobs.size() || bad_jobs.size() == 0) {
-            foreach (good_jobs, j) {
-                string lot_number(good_jobs[j]->base.job_info.data.text);
-                if (_canJobRunOnTheMachine(good_jobs[j],
-                                           candidate_machines[i])) {
-                    _job_can_run_machines[lot_number].push_back(string(
-                        candidate_machines[i]->base.machine_no.data.text));
-
-                    suitable_machines[lot_number].push_back(string(
-                        candidate_machines[i]->base.machine_no.data.text));
-                    used = true;
-                }
+        if (selected) {
+            if (_addNewResource(candidate_machines[i], part_id,
+                                _machines_wires)) {
+                --number_of_tools;
             }
-            good_jobs += ngood_jobs;  // update the good_jobs container
-
-            // update the tool and wire carried by the machines
-            // update tool
-            if (used || ngood_jobs.size()) {  // if the machine is chosen in
-                                              // first round or second round
-                if (_addNewResource(candidate_machines[i], part_no,
-                                    _machines_tools)) {
-                    number_of_tools -= 1;
-                    // printf("%s\n",
-                    // candidate_machines[i]->base.machine_no.data.text);
-                }
-                if (_addNewResource(candidate_machines[i], part_id,
-                                    _machines_wires)) {
-                    number_of_wires -= 1;
-                }
+            if (_addNewResource(candidate_machines[i], part_no,
+                                _machines_tools)) {
+                --number_of_wires;
             }
+            minimum_available_machine_set.insert(candidate_machines[i]);
         }
-        bad_jobs = nbad_jobs;
+        bad_jobs = nbad_jobs;  // update bad_jobs
     }
+    // vector<machine_t*>
+    // minimum_available_machine(minimum_available_machine_set.begin(),
+    // minimum_available_machine_set.end()); for(i = 0; i <
+    // minimum_available_machine.size(); ++i){
+    //
+    // }
+
+
+
+    // for(i = 0; i < candidate_machines.size() && number_of_wires > 0 &&
+    // number_of_tools > 0; ++i){
+    //     bool selected = false;
+    //     string machine_no(candidate_machines[i]->base.machine_no.data.text);
+    //     if(minimum_available_machine_set.count(candidate_machines[i]) == 0){
+    //         for(int j = 0; j < good_jobs.size(); ++j){
+    //             if(_canJobRunOnTheMachine(good_jobs[j],
+    //             candidate_machines[i])){
+    //                 selected = true;
+    //                 string lot_number(good_jobs[j]->base.job_info.data.text);
+    //                 _job_can_run_machines[lot_number].push_back(machine_no);
+    //             }
+
+    //         }
+    //     }
+
+    //     if(selected){
+    //         can_run_machines.push_back(machine_no);
+    //         if(_addNewResource(candidate_machines[i], part_no,
+    //         _machines_tools)){
+    //             --number_of_tools;
+    //         }
+    //         if(_addNewResource(candidate_machines[i], part_id,
+    //         _machines_wires)){
+    //             --number_of_wires;
+    //         }
+    //     }
+    // }
+
+
+
+    // after doing so, I got the minimum available machines
+    // int i = 0;
+    // for (i = 0; i < candidate_machines.size() && number_of_tools > 0 &&
+    //             number_of_wires > 0;
+    //      ++i) {
+    //     vector<job_t *> nbad_jobs;
+    //     vector<job_t *> ngood_jobs;
+    //     foreach (bad_jobs, j) {
+    //         string lot_number(bad_jobs[j]->base.job_info.data.text);
+    //         if (_canJobRunOnTheMachine(bad_jobs[j], candidate_machines[i])) {
+    //             _job_can_run_machines[lot_number].push_back(
+    //                 string(candidate_machines[i]->base.machine_no.data.text));
+
+    //             suitable_machines[lot_number].push_back(
+    //                 string(candidate_machines[i]->base.machine_no.data.text));
+
+    //             ngood_jobs.push_back(bad_jobs[j]);
+    //         } else {
+    //             nbad_jobs.push_back(bad_jobs[j]);
+    //         }
+    //     }
+
+    //     string model_name(candidate_machines[i]->base.machine_no.data.text);
+    //     bool used = false;  // a flag to describe if the machine is choose
+    //     below
+    //     // if ngood_jobs has jobs means that the machine is a good machine
+    //     if (ngood_jobs.size() || bad_jobs.size() == 0) {
+    //         foreach (good_jobs, j) {
+    //             string lot_number(good_jobs[j]->base.job_info.data.text);
+    //             if (_canJobRunOnTheMachine(good_jobs[j],
+    //                                        candidate_machines[i])) {
+    //                 _job_can_run_machines[lot_number].push_back(string(
+    //                     candidate_machines[i]->base.machine_no.data.text));
+
+    //                 suitable_machines[lot_number].push_back(string(
+    //                     candidate_machines[i]->base.machine_no.data.text));
+    //                 used = true;
+    //             }
+    //         }
+    //         good_jobs += ngood_jobs;  // update the good_jobs container
+
+    //         // update the tool and wire carried by the machines
+    //         // update tool
+    //         if (used || ngood_jobs.size()) {  // if the machine is chosen in
+    //                                           // first round or second round
+    //             if (_addNewResource(candidate_machines[i], part_no,
+    //                                 _machines_tools)) {
+    //                 number_of_tools -= 1;
+    //                 // printf("%s\n",
+    //                 // candidate_machines[i]->base.machine_no.data.text);
+    //             }
+    //             if (_addNewResource(candidate_machines[i], part_id,
+    //                                 _machines_wires)) {
+    //                 number_of_wires -= 1;
+    //             }
+    //         }
+    //     }
+    //     bad_jobs = nbad_jobs;
+    // }
     group->number_of_tools = number_of_tools;
     group->number_of_wires = number_of_wires;
     group->orphan_jobs += bad_jobs;
@@ -952,7 +1034,7 @@ void machines_t::chooseMachinesForGroups()
     // favorite machines, they won't be allowed to choose the machine in
     // stage 2. In the second stage choose the machine whose available time is
     // bigger then threshold
-    foreach (_jobs_groups, i) {
+    for (int i = 0; i < _jobs_groups.size(); ++i) {
         _jobs_groups[i]->jobs.clear();
         _chooseMachinesForAGroup(_jobs_groups[i], in_the_range_machines);
         if (_jobs_groups[i]->orphan_jobs.size()) {
@@ -965,6 +1047,11 @@ void machines_t::chooseMachinesForGroups()
             printf("[%s]-[%s] loss : %lu\n", _jobs_groups[i]->part_no.c_str(),
                    _jobs_groups[i]->part_id.c_str(),
                    _jobs_groups[i]->orphan_jobs.size());
+            for (int j = 0; j < _jobs_groups[i]->orphan_jobs.size(); ++j) {
+                printf(
+                    "\t%s\n",
+                    _jobs_groups[i]->orphan_jobs[j]->base.job_info.data.text);
+            }
         }
     }
 
